80cc8da2e6e632a5c6b29183890a2c4e092478ee
Little Ox from Osasco <77687192+pyString@users.noreply.github.com>
2022-12-22 15:03:31 -0300

    Add files via upload

---
+++ idioticTESTS.java
@@ -0,0 +1,12 @@
+public class idioticTESTS {
+    public static void main(String[] args)
+    {
+        int ptoduct = 3;
+
+        while (ptoduct <= 100)
+        {
+            ptoduct = 2 * ptoduct;
+            System.out.println(ptoduct);
+        }
+    }
+}
